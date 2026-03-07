d52a3bf14e007c7ffc050cc17bce93f620bb8533
Bjarne <kuukskee@gmail.com>
2026-03-07 21:56:46 +0100

    join command fixed

--- index.js
+++ index.js
@@ -916,11 +916,11 @@ guildId: interaction.guild.id,
 adapterCreator: interaction.guild.voiceAdapterCreator
 });
 
-await new Promise(r => setTimeout(r, 1500));
+await new Promise(r => setTimeout(r, 800));
 
 connection.destroy();
 
-await new Promise(r => setTimeout(r, 1000));
+await new Promise(r => setTimeout(r, 800));
 
 }
 await interaction.editReply(`✅ Klaar! Ik ben **${amount} keer** gejoined en geleaved in ${channel}.`);
